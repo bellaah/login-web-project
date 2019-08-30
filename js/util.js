d633fe9dc97f28ce1867f995b3538cb66026eec0
heeesun <gmltjs1996@naver.com>
2019-08-30 10:02:56 +0900

    add util.js

--- js/util.js
+++ js/util.js
@@ -5,3 +5,18 @@ const popLayer = () => {
 const closeLayer = () => {
     document.querySelector("#search_layer").style.display='none';
 }
+
+const focusIn = () => {
+    document.querySelector(".tag_div").style.borderColor='#2cb400';
+}
+
+const focusOut = () => {
+    document.querySelector(".tag_div").style.borderColor='#dadada';
+}
+
+const RemoveItself = (elem) => {
+    let item = elem.parentNode;
+    let parent = item.parentNode;
+    parent.removeChild(item);
+}
+
