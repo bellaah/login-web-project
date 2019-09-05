d2627d93a1975c3b8aabe223109ae8baed36eb78
heeesun <gmltjs1996@naver.com>
2019-09-05 23:15:21 +0900

    feat: use lowdb, login when requested by user resolves #4 #5 #7

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const url = require('url');
-const fs = require('fs');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
-var password;
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,46 +18,42 @@ app.set('view engine', 'pug');
 //요청이 실행됨 (http 요청이 오면 app이 처리한다.)
 app.use(logger('dev'));   
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));   //public에 있는 html로 접근할 시 바로 보여준다.
+app.engine('html', require('pug').renderFile);
 
-app.get('/', (req, res) => {
-  res.send(req.cookies.password);
-  res.render('index', { password: req.cookies.password });
+db.defaults({ users:[]})
+  .write()
+
+app.post('/userCheck', (req, res) => {    //로그인 시도를 하면 db에서 확인 후 redirect해준다.
+  let isUser = db.get('users')
+  .find({email: req.body.email, password:req.body.password})
+  .value();
+  console.log(isUser);
+
+  if(isUser === undefined){
+    res.send(false);
+  }else{
+    res.cookie('name', isUser.name);
+    res.send(true);
+  }
 });
 
 app.post('/main', (req, res) => {
-  password = req.body.password;
-  res.send(password + "s");
-  res.cookie('password', password);
-  res.redirect('/');
+  res.cookie('id', req.body.id);
+  res.send("!!");
 });
 
-
-
-
-
-
-app.use(express.static(path.join(__dirname, 'public')));   //public에 있는 html로 접근할 시 바로 보여준다.
-
-app.engine('html', require('pug').renderFile);
+app.post('/registerUser', (req, res) => {
+  console.log(req.body);
+  db.get('users')
+  .push(req.body)
+  .write()
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.get('/a', (req,res,next) =>{    //get 메소드를 처리하는 미들웨어 (req로 쿠키를 읽어올 수 있다.)
-  res.send("");
-})
-
-const middle = (req,res,next) => {
-  console.log("real middleware");
-  next();
-}
-
-app.get('/b', middle,(req,res,next) =>{    //get 메소드를 처리하는 미들웨어 (req로 쿠키를 읽어올 수 있다.)
-  console.log("#");   //res를 안보냈기때문에 여기에 멈춰있다.
-  next();   //next를하면 아래로 코드가 넘어간다.
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -65,12 +61,10 @@ app.use(function(req, res, next) {
 });
 
 // error handler  
-app.use(function(err, req, res, next) {   //에러 핸들러는 파라미터가 4개다.
-  // set locals, only providing error in development
+app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
