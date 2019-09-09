3d76d20c0c4c4de8dfab97c3d44117f714711a86
heeesun <gmltjs1996@naver.com>
2019-09-09 16:37:14 +0900

    feat: delete session after 1 hour

--- routes/dbCheck.js
+++ routes/dbCheck.js
@@ -1,25 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var cookieParser = require('cookie-parser');
 const crypto = require('crypto');
 var db = require('./db.js');
 
+/**
+ * 로그인 버튼을 누르면 db에 등록된 user인지 확인한다.
+ * 넘겨받은 email과 password를 isUndefined()메서드의 파라미터로 줘서 확인하게 한다.
+ */
 router.post('/userCheck', (req, res) => {   
-  let isUser = db.get('users')
-  .find({email: req.body.email, password: crypto.createHash('sha512').update(req.body.password).digest('base64')})
-  .value();
-
-  isUndefined(isUser,res);
+  let jsonData = {email: req.body.email, password: crypto.createHash('sha512').update(req.body.password).digest('base64')};
+  isUndefined(jsonData,res);
 });
 
+/**
+ * 회원가입 시, 입력한 "아이디"가 이미 존재하는지 확인한다.
+ * 넘겨받은 id를 isUndefined()메서드의 파라미터로 줘서 확인하게 한다.
+ */
 router.post('/duplicateCheck', (req, res) => {
-  let isUser = db.get('users')
-  .find({id:req.body.id})
-  .value();
-
-  isUndefined(isUser,res);
+  isUndefined({id:req.body.id},res);
 });
 
+/**
+ * 모든 input을 입력하고 회원가입을 요청하면 그 data를 db에 저장한다.
+ */
 router.post('/registerUser', (req, res) => {
   req.body.password = crypto.createHash('sha512').update(req.body.password).digest('base64');
   db.get('users')
@@ -29,7 +32,16 @@ router.post('/registerUser', (req, res) => {
   res.send("success");
 });
 
-const isUndefined = (isUser,res) => {
+/**
+ * lowdb find를 이용해 해당 data가 있는지 확인하는 메서드
+ * @param {JSON} jsonData : db에서 확인해야 할 조건이 들어있는 json 객체이다.
+ * @param {JSON} res : res 객체이다.
+ */
+const isUndefined = (jsonData,res) => {
+  let isUser = db.get('users')
+  .find(jsonData)
+  .value();
+
   if(isUser === undefined){
     res.send(false);
   }else{
--- routes/main.js
+++ routes/main.js
@@ -3,11 +3,15 @@ var express = require('express');
 var router = express.Router();
 var cookieParser = require('cookie-parser');
 const uuidv1 = require('uuid/v1');
-const crypto = require('crypto');
 
 router.use(cookieParser());
 
-router.all('/', function(req, res, next) {
+/**
+ * /main 으로 요청이 들어왔을 때, cookie가 있는지 없는지 확인하여 다른 화면을 보여준다.
+ * checkLogin()메서드를 통해 쿠키가 있는지 확인한다.
+ * cookie가 있으면 userMain을 렌더링하고 없으면 guestMain을 렌더링한다.
+ */
+router.all('/', (req, res, next) => {
   if(checkLogin(req.cookies.uuid)){
     let sessionData = db.get('session')
     .find({uuid : req.cookies.uuid})
@@ -18,7 +22,11 @@ router.all('/', function(req, res, next) {
   }
 });
 
-router.get('/userToGuest', function(req, res, next) {
+/**
+ * user가 로그아웃 버튼을 누르면 요청이 들어온다.
+ * cookie와 session을 지우고 main으로 redirect한다.
+ */
+router.get('/userToGuest', (req, res, next) => {
   res.clearCookie("uuid");
   db.get('session')
   .remove({uuid : req.cookies.uuid})
@@ -27,12 +35,19 @@ router.get('/userToGuest', function(req, res, next) {
   res.redirect('/main');
 });
 
+/**
+ * 회원가입을 끝낸 user를 main으로 이동하게 한다.
+ * main으로 이동하기 전, cookie와 session을 설정한다.
+ */
 router.post('/signupToMain', (req, res) => {
   setCookieAndSession(res, req.body.name);
 
   res.redirect('/main');
 });
 
+/**
+ * user가 로그인할 때, cookie와 session을 설정한다.
+ */
 router.post('/signinToMain', async(req, res) => {
   let userJSON = db.get('users')
   .find({email : req.body.email})
@@ -42,15 +57,44 @@ router.post('/signinToMain', async(req, res) => {
   res.redirect('/main');
 });
 
-const setCookieAndSession = async(res,name) => {    //시간 지나면 session도 같이 없어지게 처리해야함, 시간은 1시간으로 변경할 것
+/**
+ * cookie와 session을 설정하는 메서드
+ * cookie, session의 만료 시간은 1시간이다.
+ * cookie는 expires로 시간을 설정하고, session은 setInterval를 사용하여 10분마다 cookie가 있는지 확인한다.
+ * 확인했을 때, cookie가 없으면 session 또한 삭제된다.
+ * @param {JSON} res 
+ * @param {string} name 
+ */
+const setCookieAndSession = (res,name) => {    //시간 지나면 session도 같이 없어지게 처리해야함, 시간은 1시간으로 변경할 것
   let key = uuidv1();
-  res.cookie('uuid',key,{expires : new Date(Date.now()+10000)}); 
+  res.cookie('uuid',key,{expires : new Date(Date.now()+3600000)}); 
 
   db.get('session')
-  .push({uuid : key, name : name})
-  .write();
+  .push({uuid : key, name : name, expires : (new Date().valueOf()+3600000)})
+  .write()
 }
 
+/**
+ * 세션의 expires 시간과 현재 시간을 비교해 시간이 지났으면 session의 data를 삭제한다.
+ */
+const checkSessionExpires = (() => {
+  setInterval(() => {
+    let sessionData = db.get('session').value();
+    sessionData.forEach(elem => {
+      if(elem.expires <= new Date().valueOf()){
+        db.get('session')
+        .remove({uuid : elem.uuid})
+        .write();
+      }
+    });
+  },600000)
+})
+checkSessionExpires();
+
+/**
+ * cookie가 있으면 true, 없으면 false를 return
+ * @param {*} cookie 
+ */
 const checkLogin = (cookie) => {
   return cookie === undefined ? false : true;
 }
